52d638410588bb27f11ddeb45be5df71281f7aab
kurian-t <kuriant@protonmail.com>
2026-09-25 10:05:56 -0400

    Ghost

--- client/pages/BossProfile.tsx
+++ client/pages/BossProfile.tsx
@@ -43,7 +43,7 @@ import {
 import { LocationValue, EMPTY_LOCATION, declaredPayload, orUserGeo } from "@/lib/location";
 import { fetchGeo } from "@/lib/geo";
 import { useFormDraft, clearFormDraft } from "@/hooks/useFormDraft";
-
+ 
 const RATING_CATEGORIES = [
   "Communication Style",
   "Perceived Approachability",
